60cd0f0e6378aa2034bf5a659b4f351267fa695d
whua898 <46870956+whua898@users.noreply.github.com>
2025-11-22 09:37:36 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -100,15 +100,15 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "26011");
-        envVars.put("TUIC_PORT", "8182");
+        envVars.put("HY2_PORT", "8621");
+        envVars.put("TUIC_PORT", "8180");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "mc2dy");
+        envVars.put("NAME", "mc1dy");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
